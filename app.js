1710ab27bfa8dd2d778f6cc64865ef5eb2ca5ac4
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-08 18:20:33 -0500

    Made server

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //Importing the http module to launch a http node server
-const http = require();
+const http = require('http');
+
+//This function is called everytime a request reaches our server
+const server = http.createServer((req, res) => {
+    console.log(req)
+});
+
+//MAkes node.js listen for incoming requests
+server.listen(3000);
