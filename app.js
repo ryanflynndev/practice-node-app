4568918c82ffdd729d8be84da6bf50187c233a5b
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-11 14:17:43 -0500

    Learned how to parse buffer data

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 //Importing the http module to launch a http node server
 const http = require('http');
+const fs = require('fs')
 
 //This function is called everytime a request reaches our server
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message);
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body>Hello!</body>');
     res.write('</html>');
     res.end();
+
 });
 
 //MAkes node.js listen for incoming requests
