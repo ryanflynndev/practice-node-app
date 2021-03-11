0ebba32d869201bcafb3f422c6cadd4ac61d9461
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-11 12:18:29 -0500

    Added a response

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const http = require('http');
 
 //This function is called everytime a request reaches our server
 const server = http.createServer((req, res) => {
-    console.log(req);
-    process.exit();
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>Hello!</body>');
+    res.write('</html>');
+    res.end();
 });
 
 //MAkes node.js listen for incoming requests
