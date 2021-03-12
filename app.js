0466107986185cdb493d26a40af40cc889d78391
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-12 16:44:14 -0500

    Finished views

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 
-const rootDir = require('./helpers/path');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
-
-
-//Notes
-//Middleware is just a bunch of functions that are called on a request before sending a response
-//Next makes the request go to the next piece of middleware
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,22 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-const rootDir = require('../helpers/path');
-
 router.get('/', (req, res, next) => {
-    //dirname holds absolute path to this folder (routes folder)
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
